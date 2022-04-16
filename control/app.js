d31bf78e7bcd2e7217cf9ee7f258086169cab399
LAPTOP-DNOGP574\AS <a0952742534@gmail.com>
2022-04-16 21:22:10 +0800

    修改nav路徑、更新首頁craousel

--- control/app.js
+++ control/app.js
@@ -1,20 +1,21 @@
 const express=require("express");
 const app=express();
 
+app.set('views', "C:/Users/AS/Desktop/Immortal-1" + '/views');
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
 
 //homepage
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    res.render("index")
 })
 
 
 
 //login
 app.get("/login",(req,res)=>{
-    res.render("Login.ejs");
+    res.render("Login");
 })
 
 app.listen(process.env.PORT || 3000, 
