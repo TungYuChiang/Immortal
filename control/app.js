5e56ef13f1997d8e10d8cb9af449ad6598559e74
Jackson <>
2022-04-20 17:42:03 +0800

    更改又生本地端那行

--- control/app.js
+++ control/app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 
-app.set('views', "C:/Users/AS/Desktop/Immortal-1" + '/views');
+
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
